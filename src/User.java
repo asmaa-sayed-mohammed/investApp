08408b308c023e1a2be3e7d78979614924087bc6
Lenovo <mnagham17@gmail.com>
2025-05-10 20:50:34 +0300

    User class

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User   {
+    //neww
+    String email;
+    String password;
+    String uniqueusername;
+    String name;
+    public User() {}
+    public User(String email, String password, String uniqueusername, String type) {
+        this.email = email;
+        this.password = password;
+        this.uniqueusername = uniqueusername;
+    }
+}
