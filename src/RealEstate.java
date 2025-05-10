c3478120215a431fb4e00c402f63f782dc5f6b93
Lenovo <mnagham17@gmail.com>
2025-05-10 18:20:43 +0300

    Assets handling

---
+++ src/RealEstate.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class RealEstate extends Asset {
+    private String type; // Owned or Investment
+
+    public RealEstate(String name, float quantity, float purchasePrice, Date purchaseDate, String choice) {
+        super(name, "", quantity, purchasePrice, purchaseDate);
+        this.type = chooseType(choice);
+        super.setAssetType(this.type + " RealEstate");
+    }
+
+    private String chooseType(String choice) {
+        if (choice.equals("1")) {
+            return "Investment";
+        } else {
+            return "Owned";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[RealEstate - " + type + "] " + super.toString();
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
