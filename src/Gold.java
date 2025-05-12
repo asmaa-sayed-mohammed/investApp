e91e41842451931d7fd3ffaaadb18b2b41567b48
Mennamohamedashour188 <162712725+Mennamohamedashour188@users.noreply.github.com>
2025-05-12 16:04:02 +0300

    Add files via upload

--- src/Gold.java
+++ src/Gold.java
@@ -1,27 +1,33 @@
 import java.util.Date;
-
+import java.util.Scanner;
 public class Gold  extends Asset {
-    private String type; // Owned or Investment
 
-    public Gold(String name, float quantity, float purchasePrice, Date purchaseDate, String choice , double amount) {
-        super(name, "", quantity, purchasePrice, purchaseDate);
-        this.type = chooseType(choice);
-        super.setAssetType(this.type + " Gold");
-        super.setAssetamount(amount);
-    }
 
-    private String chooseType(String choice) {
-        if (choice.equals("1")) {
-            return "Investment";
-        } else {
-            return "Owned";
+        public Gold(String name, float quantity, float purchasePrice, Date purchaseDate, String choice , double amount) {
+            super(name, "", quantity, purchasePrice, purchaseDate);
+            this.investType = chooseType(choice);
+            super.setAssetType(this.investType + " Gold");
+            super.setAssetamount(amount);
         }
-    }
 
-    @Override
-    public String toString() {
-        return "[Gold - " + type + "] " + super.toString();
-    }
+        private String chooseType(String choice) {
+            if (choice.equals("1")) {
+                return "Investment";
+            } else {
+                return "Owned";
+            }
+        }
+        public void setType() {
+             Scanner scanner = new Scanner(System.in);
+            System.out.print("is this Gold: 1)Investment          2)Owned ");
+            String choice3 = scanner.nextLine();
+            this.investType = chooseType(choice3) ;
+            super.setAssetType(this.investType + " Gold");
+        }
+        @Override
+        public String toString() {
+            return "[Gold - " + investType + "] " + super.toString();
+        }
 
 
 
