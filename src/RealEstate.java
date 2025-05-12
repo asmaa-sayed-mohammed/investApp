cada5cf80f10dbbd83d2516cc42491ff80194e35
Mennamohamedashour188 <162712725+Mennamohamedashour188@users.noreply.github.com>
2025-05-12 16:03:31 +0300

    Add files via upload

--- src/RealEstate.java
+++ src/RealEstate.java
@@ -1,28 +1,33 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class RealEstate extends Asset {
-    private String type; // Owned or Investment
 
     public RealEstate(String name, float quantity, float purchasePrice, Date purchaseDate, String choice) {
         super(name, "", quantity, purchasePrice, purchaseDate);
-        this.type = chooseType(choice);
-        super.setAssetType(this.type + " RealEstate");
+        this.investType = chooseType(choice);
+        super.setAssetType(this.investType+ " RealEstate");
     }
 
     private String chooseType(String choice) {
         if (choice.equals("1")) {
             return "Investment";
         } else {
-            return " Owned ";
+            return "Owned";
         }
     }
+    public void setType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("is this RealEstate: 1)Investment          2)Owned ");
+        String choice3 = scanner.nextLine();
 
+        this.investType=chooseType(choice3) ;
+        super.setAssetType(this.investType+ " RealEstate");
+    }
     @Override
     public String toString() {
-        return "[RealEstate - " + type + "] " + super.toString();
+                return "[RealEstate - " + investType+ "] " + super.toString();
     }
 
-    public String getType() {
-        return this.type;
-    }
+
 }
