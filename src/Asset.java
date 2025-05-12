76ecec2ef9159855bcca95cf8d3f5e55bc81d2c5
menna mohamed <mm1881569@gmail.com>
2025-05-12 18:58:48 +0300

    update.

--- src/Asset.java
+++ src/Asset.java
@@ -36,59 +36,108 @@ public class Asset implements Serializable {
         this.investType="Investment";
     }
 
+    /** @return The name of the asset. */
     public String getAssetName() {
         return this.assetName;
     }
 
+    /** @return The quantity of the asset. */
     public float getQuantity() {
         return this.quantity;
     }
 
+    /** @return The date the asset was purchased. */
     public Date getPurchaseDate() {
         return this.purchaseDate;
     }
 
+    /** @return The price at which the asset was purchased. */
     public float getPurchasePrice() {
         return this.purchasePrice;
     }
 
+    /**
+     * Sets the quantity of the asset.
+     *
+     * @param quantity The new quantity.
+     */
     public void setQuantity(float quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Sets the purchase price of the asset.
+     *
+     * @param purchasePrice The new purchase price.
+     */
     public void setPurchasePrice(float purchasePrice) {
         this.purchasePrice = purchasePrice;
     }
 
+    /**
+     * Sets the name of the asset.
+     *
+     * @param assetName The new asset name.
+     */
     public void setAssetName(String assetName) {
         this.assetName = assetName;
     }
 
+    /**
+     * Sets the purchase date of the asset.
+     *
+     * @param purchaseDate The new purchase date.
+     */
     public void setPurchaseDate(Date purchaseDate) {
         this.purchaseDate = purchaseDate;
     }
 
+    /**
+     * Sets the type of the asset.
+     *
+     * @param assetType The new asset type (e.g., stock, gold).
+     */
     public void setAssetType(String assetType) {
         this.assetType = assetType;
     }
 
+    /** @return The type of the asset. */
     public String getAssetType() {
         return this.assetType;
     }
-    public double getAssetamount() {
 
+    /** @return The total amount/value of the asset. */
+    public double getAssetamount() {
         return this.amount;
     }
 
+    /**
+     * Sets the total amount/value of the asset.
+     *
+     * @param amount The total calculated asset value.
+     */
     public void setAssetamount(double amount) {
         this.amount = amount;
     }
 
+    /** @return The investment type label of the asset. */
     public String getInvestType() {
         return this.investType;
     }
+
+    /**
+     * Returns a string representation of the asset including its key details.
+     *
+     * @return A string containing asset details.
+     */
+    @Override
     public String toString() {
-        String var10000 = this.assetName;
-        return "Asset Name: " + var10000 + ", Type: " + this.assetType + ", Quantity: " + this.quantity + ", Purchase Price: " + this.purchasePrice + ", Purchase Date: " + (new SimpleDateFormat("yyyy-MM-dd")).format(this.purchaseDate) + ", Asset Amount: " + this.amount +", Asset Investment type: "+this.investType ;
+        return "Asset Name: " + this.assetName +
+                ", Type: " + this.assetType +
+                ", Quantity: " + this.quantity +
+                ", Purchase Price: " + this.purchasePrice +
+                ", Purchase Date: " + new SimpleDateFormat("yyyy-MM-dd").format(this.purchaseDate) +
+                ", Asset Amount: " + this.amount +
+                ", Asset Investment type: " + this.investType;
     }
 }
