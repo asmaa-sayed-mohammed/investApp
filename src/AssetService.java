2d2c8372170fc77d45bc4665303d23a66f3feea6
menna mohamed <mm1881569@gmail.com>
2025-05-12 19:00:42 +0300

    update.

--- src/AssetService.java
+++ src/AssetService.java
@@ -1,10 +1,36 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-interface AssetService {
+/**
+ * The {@code AssetService} interface defines the contract for managing a collection of {@link Asset} objects.
+ * It includes methods for adding, editing, and removing assets, as well as validating asset indices.
+ */
+public interface AssetService {
+
+    /**
+     * Adds a new asset to the collection.
+     *
+     * @param var1 The {@link Asset} object to be added.
+     */
     void addAsset(Asset var1);
 
+    /**
+     * Edits an existing asset at the specified index.
+     *
+     * @param var1 The index of the asset to be edited.
+     */
     void editAsset(int var1);
 
+    /**
+     * Removes an asset from the collection based on the provided index.
+     *
+     * @param var1 The index of the asset to be removed.
+     */
     void removeAsset(int var1);
+
+    /**
+     * Checks if the provided index is valid for the asset list.
+     *
+     * @param var1 The index to validate.
+     * @return {@code true} if the index is valid; {@code false} otherwise.
+     */
 
     boolean isValidIndex(int var1);
 }
