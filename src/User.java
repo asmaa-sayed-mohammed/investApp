3974ca559d6f6dd0d90528868c625d046915e22f
menna mohamed <mm1881569@gmail.com>
2025-05-12 20:05:24 +0300

    update.

--- src/User.java
+++ src/User.java
@@ -1,10 +1,27 @@
+/**
+ * The {@code User} class represents a user with authentication-related information
+ * such as email, password, unique username, and name.
+ * <p>
+ * This class is used as a base for login and signup operations.
+ */
 public class User   {
     //neww
     String email;
     String password;
     String uniqueusername;
     String name;
+    /**
+     * Default no-argument constructor.
+     */
     public User() {}
+    /**
+     * Constructs a {@code User} with the specified email, password, and unique username.
+     *
+     * @param email the user's email address
+     * @param password the user's password
+     * @param uniqueusername the user's unique username
+     * @param type an unused parameter (likely a remnant or placeholder)
+     */
     public User(String email, String password, String uniqueusername, String type) {
         this.email = email;
         this.password = password;
